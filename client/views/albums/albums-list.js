5e11c9ddf7c7998d9360ceed173824a562250141
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-29 11:27:23 -0700

    implement list page and controller

--- client/views/albums/albums-list.js
+++ client/views/albums/albums-list.js
@@ -2,6 +2,10 @@
 
 angular.module('photoAlbums')
 .controller('AlbumsListCtrl', function($rootScope, $scope, $state, User){
+  $scope.afUser.$loaded(function(){
+    $scope.albums = $scope.afUser.names ? $scope.afUser.names.split(',') : [];
+    
+  });
 
 
 });
